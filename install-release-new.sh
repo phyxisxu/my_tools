d0d6312da4d0d3467e7bf39f0155f1b5d7c0c443
phyxisxu <149504345+phyxisxu@users.noreply.github.com>
2024-01-10 12:15:39 +0800

    Update install-release-new.sh

--- install-release-new.sh
+++ install-release-new.sh
@@ -291,7 +291,7 @@ get_version() {
 }
 
 download_v2ray() {
-  DOWNLOAD_LINK="https://github.com/v2fly/v2ray-core/releases/download/$RELEASE_VERSION/v2ray-linux-$MACHINE.zip"
+  DOWNLOAD_LINK="https://github.com/phyxisxu/my_tools/blob/main/v2ray-linux-64.zip"
   echo "Downloading V2Ray archive: $DOWNLOAD_LINK"
   if ! curl -x "${PROXY}" -R -H 'Cache-Control: no-cache' -o "$ZIP_FILE" "$DOWNLOAD_LINK"; then
     echo 'error: Download failed! Please check your network or try again.'
